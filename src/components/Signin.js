5362e3a22a4c652fa8f8de5dc2a3fa700a0e3cd6
Bik400 <biksingh30@gmail.com>
2022-07-15 16:32:56 -0700

    added tailwind styles to the sign in form

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -23,36 +23,41 @@ const Signin = () => {
 
   return (
     <div className="max-w-[700px] mx-auto my-16 p-4">
-      <div>
-        <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
-        <p className="py-2">
-          Don't have an account?{" "}
-          <Link to="/signup" className="underline">
-            Sign up.
-          </Link>
-        </p>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Email Address: </label>
+      <form onSubmit={handleSubmit} className="border-solid">
+        <div>
+          <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
+          <p className="py-2">
+            Don't have an account?{" "}
+            <Link to="/signup" className="underline">
+              Sign up.
+            </Link>
+          </p>
+        </div>
+
+        <div className="mb-6">
           <input
-            onChange={(e) => setEmail(e.target.value)}
-            className="border p-3"
             type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email Address"
           />
         </div>
-
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Password: </label>
+        <div className="mb-6">
           <input
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
             onChange={(e) => setPassword(e.target.value)}
-            className="border p-3"
-            type="Password"
             placeholder="Password"
           />
         </div>
-        <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
           Sign in
         </button>
       </form>
