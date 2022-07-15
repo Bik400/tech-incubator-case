f97cb87375268fb5fa849d246b298f19f23deea5
Bik400 <biksingh30@gmail.com>
2022-07-15 16:33:39 -0700

    added tailwind styles to sign up form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -34,26 +34,30 @@ const Signup = () => {
         </p>
       </div>
       <form onSubmit={handleSubmit}>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Email Address: </label>
+        <div className="mb-6">
+          {/* <label className="block mb-2 text-sm font-medium text-black">
+            Email Address:{" "}
+          </label> */}
           <input
             onChange={(e) => setEmail(e.target.value)}
-            className="border p-3"
+            class="bg-gray-50 border border-gray-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             type="email"
             placeholder="Email Address"
           />
         </div>
 
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Password: </label>
+        <div className="mb-6">
+          {/* <label className="block mb-2 text-sm font-medium text-black">
+            Password:{" "}
+          </label> */}
           <input
             onChange={(e) => setPassword(e.target.value)}
-            className="border p-3"
+            class="bg-gray-50 border border-gray-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             type="Password"
             placeholder="Password"
           />
         </div>
-        <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Sign up
         </button>
       </form>
