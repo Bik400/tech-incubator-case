bf312e5f11c5ceb197ec3ba48605128dd66cb1fc
Bik400 <biksingh30@gmail.com>
2022-07-15 16:31:37 -0700

    added the logout button to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const [activeTab, setActiveTab] = useState("Main");
-  const location = useLocation();
+  const { logout } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+      console.log("You are logged out");
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   return (
-    <div className="header">
+    <div className="flex flex-row justify-between p-6 align-middle items-center">
       <p className="logo">Tech Incubator</p>
-      <div className="header-right">
-        <Link to="mainpage">
-          <p
-            className={`${activeTab === "Main" ? "active" : ""}`}
-            onClick={() => setActiveTab("Main")}
-          >
-            Main Page
-          </p>
+      <div className="flex flex-row justify-between">
+        <Link to="/mainpage">
+          <p className="pr-4">Main Page</p>
         </Link>
+        <button
+          className="py-1.5 px-3 mr-1 mb-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
