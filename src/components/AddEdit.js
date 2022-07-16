2092e0d1ff1a08587677bca8cc12ad250969820b
Bik400 <biksingh30@gmail.com>
2022-07-16 14:44:58 -0700

    the back button now navigates to listpage

--- src/components/AddEdit.js
+++ src/components/AddEdit.js
@@ -68,7 +68,7 @@ const AddEdit = () => {
         });
       }
 
-      setTimeout(() => navigate("/mainpage"), 500);
+      setTimeout(() => navigate("/listpage"), 500);
     }
   };
 
@@ -117,7 +117,7 @@ const AddEdit = () => {
           />
         </div>
         <div className="flex justify-between">
-          <Link to="/mainpage">
+          <Link to="/listpage">
             <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
               Go Back
             </button>
